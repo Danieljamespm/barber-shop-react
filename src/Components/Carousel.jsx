e6b0eedf2a69c3478aa8aa6c6daf515e4bd661a0
Danny Mettler <danieljamespm@gmail.com>
2025-01-16 20:31:38 -0500

    added carousel jsx file and added images to be used for carousel.

---
+++ src/Components/Carousel.jsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+
+const Carousel = () => {
+  const[currentIndex, setCurrentIndex] = useState(0)
+  
+  
+  
+  
+  
+  
+  return (
+    <div>Carousel</div>
+  )
+}
+
+export default Carousel
