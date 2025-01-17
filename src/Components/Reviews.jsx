8b95d1d4984597aab07ecc4f927ac6214a65d723
Danny Mettler <danieljamespm@gmail.com>
2025-01-17 13:56:40 -0500

    added slider from react slick.

--- src/Components/Reviews.jsx
+++ src/Components/Reviews.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
+import Slider from 'react-slick'
 
 
 const Reviews = () => {
 
+    const settings = {
+        dots: true,
+        fade: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        waitForAnimate: false
+      };
 
 
   return (
-    <div className='bg-review-image' >
+    <div className='bg-review-image h-[500px] bg-cover bg-center opacity-90 pt-10 ' >
+        <p className='text-white text-center'>1000+ HAPPY CLIENTS</p>
+        <Slider {...settings}>
+
+
+        </Slider>
 
     </div>
   )
