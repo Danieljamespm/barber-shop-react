da7d8e9c8420aff4bfd0235c2983cf2e0c996f57
Danny Mettler <danieljamespm@gmail.com>
2025-01-17 13:05:51 -0500

    took care of merging errors.

--- src/Components/Aboutus.jsx
+++ src/Components/Aboutus.jsx
@@ -5,17 +5,13 @@ const Aboutus = () => {
     <div id='aboutus' className='w-full bg-white py-8 px-4 scroll-smooth'>
         <div className='maw-w-[1240px] mx-auto grid md:grid-cols-2 font-oswald md:px-16'>
             <div className='flex flex-col justify-center'>
- main
+ 
             <p className='text-[15px] text-[#D19D64] pt-10 mb-2 md:py-2'>THE UNIQUE STORY</p>
             
             <h1 className='text-2xl sm:text-5xl font-bold'>52 YEARS OF EXPERIENCE</h1>
             <hr className='w-20 h-1 bg-[#D19D64] border-0 rounded mb-10 mt-8' />
 
-            <p className='text-[15px] text-[#D19D64] pt-10 mb-2 md:py-2'>THE UNIQUE STORY</p>
-            
-            <h1 className='text-2xl sm:text-5xl font-bold'>52 YEARS OF EXPERIENCE</h1>
-            <hr className='w-20 h-1 bg-[#D19D64] border-0 rounded mb-10 mt-8' />
- component/carousel
+          
             
             <p className='text-md pb-8 text-balance tracking-widest uppercase text-clip'>Welcome to Vintage Cuts, where tradition meets precision. With over 52 years of expertise, we take pride in offering a timeless experience in men's grooming. Specializing in vintage haircuts and expert beard trims, our skilled barbers combine old-school techniques with a modern touch, ensuring every visit feels like stepping back into an era of refined craftsmanship.</p>
             
@@ -28,11 +24,10 @@ const Aboutus = () => {
         
            
 
-< main
-        <button className='bg-[#D19D64] text-black px-6 py-2 m-5 md:mx-32 text-[9px] font-oswald  hover:bg-black hover:text-[#D19D64] hover:duration-500 rounded-lg font-bold tracking-widest'>READ MORE</button>
 
         <button className='bg-[#D19D64] text-black px-6 py-2 m-5 md:mx-32 text-[9px] font-oswald  hover:bg-black hover:text-[#D19D64] hover:duration-500 rounded-lg font-bold tracking-widest'>READ MORE</button>
- component/carousel
+
+        
 
     </div>
   )
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,19 +4,14 @@ import React from 'react'
 const Hero = () => {
   return (
     <div className='bg-hero-image bg-cover bg-center h-[850px] flex justify-center items-center flex-col font-oswald '>
- main
+ 
         <img className='w-[220px]  mx-auto' src="./barber-scissors.png" alt="" />
         <p className='text-xs md:text-sm text-white [word-spacing:0.16rem] p-2 tracking-widest font-light'>WE ARE A UNIQUE</p>
         <h1 className='text-white font-bold text-5xl sm:text-6xl md:text-9xl '>BARBER SHOP</h1>
         <p className='text-white p-2 text-md font-light tracking-widest font-Opensans'>The best place for your hairstyle.</p>
         <button className='bg-[#D19D64] p-3 m-4 text-[10px] hover:text-[#34567e] hover:duration-500 rounded-lg font-bold tracking-widest'>MAKE AN APPOINTMENT</button>
 
-       <img className='w-[220px]  mx-auto' src="./barber-scissors.png" alt="" />
-        <p className='text-xs md:text-sm text-white [word-spacing:0.16rem] p-2 tracking-widest font-light'>WE ARE A UNIQUE</p>
-        <h1 className='text-white font-bold text-5xl sm:text-6xl md:text-9xl '>BARBER SHOP</h1>
-        <p className='text-white p-2 text-md font-light tracking-widest font-Opensans'>The best place for your hairstyle.</p>
-        <button className='bg-[#D19D64] p-3 m-4 text-[10px] hover:text-[#34567e] hover:duration-500 rounded-lg font-bold tracking-widest'>MAKE AN APPOINTMENT</button>
- component/carousel
+       
         <div>
             
         </div>
