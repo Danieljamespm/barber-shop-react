a5a698f3fc39ffb126630334e421bf81fde26ca6
Danny Mettler <danieljamespm@gmail.com>
2025-01-23 22:01:58 -0500

    added slider from react slick.

---
+++ src/Components/Employees.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+import Slider from 'react-slick'
+
+const Employees = () => {
+  return (
+    <div>Employees</div>
+  )
+}
+
+export default Employees
