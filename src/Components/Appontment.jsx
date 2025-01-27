1f816d3635296477561fc5f225d6d3af206cbc88
Danny Mettler <danieljamespm@gmail.com>
2025-01-27 14:35:50 -0500

    padding changes to Opening Hours.

--- src/Components/Appontment.jsx
+++ src/Components/Appontment.jsx
@@ -7,7 +7,7 @@ const Appontment = () => {
         <img className='w-[80px] mx-auto' src='./barber-scissors.png' />
         <div className=' w-[100%] h-[60%] flex justify-center pt-7'>
         <div className='w-[25%]'>
-            <p className='font-oswald font-bold text-xl text-[#d19d64] pb-6'>OPENING HOURS</p>
+            <p className='font-oswald font-bold text-xl text-[#d19d64] pb-8'>OPENING HOURS</p>
             <p className='font-oswald text-xs pb-9 font-light'>MONDAY..............................................................9AM-6PM</p>
             <p className='font-oswald text-xs pb-9 font-light'>TUESDAY..............................................................9AM-6PM</p>
             <p className='font-oswald text-xs pb-9 font-light'>WEDNESDAY.......................................................9AM-6PM</p>
