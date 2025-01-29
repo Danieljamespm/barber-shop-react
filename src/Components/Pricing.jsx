a710802dc889a4f5976b9f5765ce4180afb5a6f6
Danny Mettler <danieljamespm@gmail.com>
2025-01-29 00:42:14 -0500

    added and styled tabs component.

--- src/Components/Pricing.jsx
+++ src/Components/Pricing.jsx
@@ -2,69 +2,161 @@ import React, {useState} from 'react'
 
 const Pricing = () => {
 
-    const [activeTab, setActiveTab] = useState(0)
+    const [activeTab, setActiveTab] = useState("tab1")
 
     const tabs = [
-        {
-            label: 'Hair',
-            services: [
-                {name:'VINTAGE HAIR CUT', price: '$12'},
-                {name: 'CLASSIC HAIR CUT', price: '25'},
-                {name: 'STYLISH HAIR CUT', price: '$34'},
-                {name: 'TRADITIONAL HAIR CUT', price: '$44'},
-            ],
-            additionalServices: [
-                {name: 'HAIR WASH', price: '$12'},
-                {name: 'HAIR COLOR', price: '75'},
-                {name: 'HIGHLIGHT', price: '49'},
-                {name: 'HAIR RESTORATION OR REPLACEMENT', price: 'STARTING AT $99'},
-            ],
-        },
-        {
-            label: 'Shave',
-            services: [
-                {name: "BEARD TRIM", price: '$10'},
-                {name: "FULL BEARD SHAPING", price:'$30'},
-                {name:"HOT TOWEL SHAVE", price:'$25'},
-                {name:'CLASSIC SHAVE', price:'$15'},
-            ],
-            additionalSerevices: [
-                {name: "BEARD LINE-UP", price: '$20'},
-                {name: "MUSTACHE TRIM", price:'$20'},
-                {name:"FACIAL HAIR TINTING", price:'$40'},
-                {name:'SKIN TREATMENTS', price:'$15'},
-            ],
-        },
-        {
-            label: 'OTHER',
-            services:[
-                {name: "MANICURE", price: '$10'},
-                {name: "PEDICURE", price:'$15'},
-                {name:"FACIAL", price:'$20'},
-                {name:'FACE MASK', price:'$10'},
-            ],
-            additionalServices: [
-                {name: "EYEBROW WAXING", price: '$20'},
-                {name: "NOSE AND EAR TRIMMING", price:'$20'},
-                {name:"CHEST AND BACK WAXING", price:'$40'},
-                {name:'MASSAGE', price:'$30'},
-            ],
-        },
+        {id:"tab1", label: "HAIR"},
+        {id:"tab2", label: "SHAVE"},
+        {id:"tab3", label: "OTHER"}
     ];
+
+    const tabContent = {
+        tab1: (
+            <div className='flex justify-around flex-col md:flex-row'>
+                <div className='bg-white w-[100%] md:w-[35%]'>
+                    
+                    <div className='flex justify-between'>
+                    <p className='font-oswald border-b-2 w-full border-gray-400 md:pt-10'>VINTAGE HAIR CUT </p>
+                    <p className='text-[#d19d64] border-b-2 md:pt-10 border-gray-400 font-oswald pt-3'>$12</p>
+                    </div>
+                    <div className='flex justify-between'>
+                    <p className='font-oswald border-b-2 w-full border-gray-400 md:pt-10'>CLASSIC HAIRCUT</p>
+                    <p className='text-[#d19d64] md:pt-10 font-oswald border-b-2 border-gray-400 pt-3'>$25</p>
+                    </div>
+                    <div className='flex justify-between'>
+                    <p className='font-oswald border-b-2 w-full border-gray-400 md:pt-10'>STYLISH HAIRCUT</p>
+                    <p className='text-[#d19d64] md:pt-10 font-oswald border-b-2 border-gray-400 pt-3'>$34</p>
+                    </div>
+                    <div className='flex justify-between'>
+                    <p className='font-oswald border-b-2 w-full border-gray-400 md:pt-10'>TRADITIONAL HAIR CUT</p>
+                    <p className='text-[#d19d64] md:pt-10 font-oswald border-b-2 border-gray-400 pt-3'>$44</p>
+                    </div>
+                </div>
+                <div className='bg-white w-[100%] md:w-[35%]'>
+                    <div className='flex justify-between'>
+                    <p className='font-oswald border-b-2 w-full border-gray-400 md:pt-10'>HAIR WASH</p>
+                    <p className='text-[#d19d64] md:pt-10 font-oswald border-b-2 border-gray-400 pt-3'>$12</p>
+                    </div>
+                    <div className='flex justify-between'>
+                    <p className='font-oswald border-b-2 w-full border-gray-400 md:pt-10'>HAIR COLOR</p>
+                    <p className='text-[#d19d64] md:pt-10 font-oswald border-b-2 border-gray-400 pt-3'>$75</p>
+                    </div>
+                    <div className='flex justify-between'>
+                    <p className='font-oswald border-b-2 w-full border-gray-400 md:pt-10'>HIGHLIGHT</p>
+                    <p className='text-[#d19d64] md:pt-10 font-oswald border-b-2 border-gray-400 pt-3'>$49</p>
+                    </div>
+                    <div className='flex justify-between'>
+                    <p className='font-oswald border-b-2 w-full border-gray-400 md:pt-10'>HAIR RESTORATION/REPLACEMENT</p>
+                    <p className='text-[#d19d64] md:pt-10  font-oswald border-b-2 border-gray-400 pt-3'>$99</p>
+                    </div>
+                </div>
+            </div>
+        ),
+        tab2: (
+            <div className='flex justify-around flex-col md:flex-row'>
+            <div className='bg-white w-[100%] md:w-[35%]'>
+                
+                <div className='flex justify-between'>
+                <p className='font-oswald border-b-2 w-full border-gray-400 md:pt-10'>BEAR TRIM </p>
+                <p className='text-[#d19d64] border-b-2 md:pt-10 border-gray-400 font-oswald pt-3'>$10</p>
+                </div>
+                <div className='flex justify-between'>
+                <p className='font-oswald border-b-2 w-full border-gray-400 md:pt-10'>FULL BEARD SHAPING</p>
+                <p className='text-[#d19d64] md:pt-10 font-oswald border-b-2 border-gray-400 pt-3'>$30</p>
+                </div>
+                <div className='flex justify-between'>
+                <p className='font-oswald border-b-2 w-full border-gray-400 md:pt-10'>HOT TOWEL SHAVE</p>
+                <p className='text-[#d19d64] md:pt-10 font-oswald border-b-2 border-gray-400 pt-3'>$25</p>
+                </div>
+                <div className='flex justify-between'>
+                <p className='font-oswald border-b-2 w-full border-gray-400 md:pt-10'>CLASSIC SHAVE</p>
+                <p className='text-[#d19d64] md:pt-10 font-oswald border-b-2 border-gray-400 pt-3'>$15</p>
+                </div>
+            </div>
+            <div className='bg-white w-[100%] md:w-[35%]'>
+                <div className='flex justify-between'>
+                <p className='font-oswald border-b-2 w-full border-gray-400 md:pt-10'>BEARD LINE-UP</p>
+                <p className='text-[#d19d64] md:pt-10 font-oswald border-b-2 border-gray-400 pt-3'>$20</p>
+                </div>
+                <div className='flex justify-between'>
+                <p className='font-oswald border-b-2 w-full border-gray-400 md:pt-10'>MUSTACHE TRIM</p>
+                <p className='text-[#d19d64] md:pt-10 font-oswald border-b-2 border-gray-400 pt-3'>$20</p>
+                </div>
+                <div className='flex justify-between'>
+                <p className='font-oswald border-b-2 w-full border-gray-400 md:pt-10'>FACIAL HAIR TINTING</p>
+                <p className='text-[#d19d64] md:pt-10 font-oswald border-b-2 border-gray-400 pt-3'>$40</p>
+                </div>
+                <div className='flex justify-between'>
+                <p className='font-oswald border-b-2 w-full border-gray-400 md:pt-10'>SKIN TREATMENTS</p>
+                <p className='text-[#d19d64] md:pt-10  font-oswald border-b-2 border-gray-400 pt-3'>$15</p>
+                </div>
+            </div>
+        </div>
+        ),
+        tab3: (
+            <div className='flex justify-around flex-col md:flex-row'>
+                <div className='bg-white w-[100%] md:w-[35%]'>
+                    
+                    <div className='flex justify-between'>
+                    <p className='font-oswald border-b-2 w-full border-gray-400 md:pt-10'>MANICURE </p>
+                    <p className='text-[#d19d64] border-b-2 md:pt-10 border-gray-400 font-oswald pt-3'>$10</p>
+                    </div>
+                    <div className='flex justify-between'>
+                    <p className='font-oswald border-b-2 w-full border-gray-400 md:pt-10'>PEDICURE</p>
+                    <p className='text-[#d19d64] md:pt-10 font-oswald border-b-2 border-gray-400 pt-3'>$15</p>
+                    </div>
+                    <div className='flex justify-between'>
+                    <p className='font-oswald border-b-2 w-full border-gray-400 md:pt-10'>FACIAL</p>
+                    <p className='text-[#d19d64] md:pt-10 font-oswald border-b-2 border-gray-400 pt-3'>$20</p>
+                    </div>
+                    <div className='flex justify-between'>
+                    <p className='font-oswald border-b-2 w-full border-gray-400 md:pt-10'>FACE MASK</p>
+                    <p className='text-[#d19d64] md:pt-10 font-oswald border-b-2 border-gray-400 pt-3'>$10</p>
+                    </div>
+                </div>
+                <div className='bg-white w-[100%] md:w-[35%]'>
+                    <div className='flex justify-between'>
+                    <p className='font-oswald border-b-2 w-full border-gray-400 md:pt-10'>EYEBROW WAXING</p>
+                    <p className='text-[#d19d64] md:pt-10 font-oswald border-b-2 border-gray-400 pt-3'>$20</p>
+                    </div>
+                    <div className='flex justify-between'>
+                    <p className='font-oswald border-b-2 w-full border-gray-400 md:pt-10'>NOSE AND EAR TRIMMING</p>
+                    <p className='text-[#d19d64] md:pt-10 font-oswald border-b-2 border-gray-400 pt-3'>$20</p>
+                    </div>
+                    <div className='flex justify-between'>
+                    <p className='font-oswald border-b-2 w-full border-gray-400 md:pt-10'>CHEST AND BACK WAXING</p>
+                    <p className='text-[#d19d64] md:pt-10 font-oswald border-b-2 border-gray-400 pt-3'>$40</p>
+                    </div>
+                    <div className='flex justify-between'>
+                    <p className='font-oswald border-b-2 w-full border-gray-400 md:pt-10'>MASSAGE</p>
+                    <p className='text-[#d19d64] md:pt-10  font-oswald border-b-2 border-gray-400 pt-3'>$30</p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 
 
 
 
   return (
-    <div className='w-[100%] h-[700px]'>
-    <div className='text-center'>
-        <h2 className='text-4xl font-oswald font-bold  pt-20 pb-2'>OUR PRICING</h2>
-        <img className='w-[80px] mx-auto' src='./barber-scissors.png' />
-    </div>
-        <div className='max-w-[900px] bg-gray-500 h-[500px] mx-auto'>
-
-        </div>
-    </div>
+    
+        <div className='h-[700px] w-full'>
+            <h2 className='text-4xl text-center font-oswald font-bold pt-20 pb-2'>OUR PRICING</h2>
+            <img className='w-[80px] mx-auto pb-10' src='./barber-scissors.png' />
+                <div className='bg-slate-300 w-[80%] h-[50%] m-auto'>
+               <div className='flex flex-wrap justify-around bg-black text-gray-500 text-xl font-bold border-b-2 border-[#d19d64] '>
+                {tabs.map((tab) => (
+                    <button key={tab.id} onClick={() => setActiveTab(tab.id)} className={`font-oswald hover:bg-[#d19d64] hover:text-black px-10 hover:duration-500 ${
+                        activeTab === tab.id ? 'bg-[#d19d64] text-black' : 'bg-black text-gray-500'
+                    }`}>
+                        
+                        {tab.label}</button>
+                ))}
+            </div>
+            <div>{tabContent[activeTab]}</div>
+            </div>
+        </div>    
   )
 }
 
