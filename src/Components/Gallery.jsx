cb749c165046be0962134a4079b98b739079d10e
Danny Mettler <danieljamespm@gmail.com>
2025-01-30 15:00:52 -0500

    added tab content, added images and styling to make them unifrom and responzive to hover.

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -16,20 +16,97 @@ import image14 from '../assets/shave-6.jpg'
 import image15 from '../assets/shave-7.jpg'
 import image16 from '../assets/shave-8.jpg'
 
-const galleryTabs ={
-    'Haircut': [
-        image1, image2, image3, image4, 
-        image5, image6, image7, image8,
-    ],
-    'Shave': [
-        image9, image10, image11, image12,
-        image13, image14, image15, image16,
-    ],
-};
+const galleryTabs = [
+    {id:'tab1', label:'HAIRCUT'},
+    {id:'tab2', label:'SHAVE'}
+]
 
-const Gallery = () => {
-    return(
-        <></>
+const tabContent = {
+    tab1: (
+        <div className=' pt-4 flex flex-wrap justify-center items-center gap-4'>
+            <div className='border-[#d19d64] bg-[#d19d64] '>
+            <img className='w-[250px] h-[250px] object-cover hover:scale-95 hover:duration-500 hover:opacity-50 ' src={image1} alt='' />
+            </div>
+            <div className='border-[#d19d64] bg-[#d19d64] '>
+            <img className='w-[250px] h-[250px] object-cover hover:scale-95 hover:duration-500 hover:opacity-50 ' src={image2} alt='' />
+            </div>
+            <div className='border-[#d19d64] bg-[#d19d64] '>
+            <img className='w-[250px] h-[250px] object-cover hover:scale-95 hover:duration-500 hover:opacity-50 ' src={image3} alt='' />
+            </div>
+            <div className='border-[#d19d64] bg-[#d19d64] '>
+            <img className='w-[250px] h-[250px] object-cover hover:scale-95 hover:duration-500 hover:opacity-50 ' src={image4} alt='' />
+            </div>
+            <div className='border-[#d19d64] bg-[#d19d64] '>
+            <img className='w-[250px] h-[250px] object-cover hover:scale-95 hover:duration-500 hover:opacity-50 ' src={image5} alt='' />
+            </div>
+            <div className='border-[#d19d64] bg-[#d19d64] '>
+            <img className='w-[250px] h-[250px] object-cover hover:scale-95 hover:duration-500 hover:opacity-50 ' src={image6} alt='' />
+            </div>
+            <div className='border-[#d19d64] bg-[#d19d64] '>
+            <img className='w-[250px] h-[250px] object-cover hover:scale-95 hover:duration-500 hover:opacity-50 ' src={image7} alt='' />
+            </div>
+            <div className='border-[#d19d64] bg-[#d19d64] '>
+            <img className='w-[250px] h-[250px] object-cover hover:scale-95 hover:duration-500 hover:opacity-50 ' src={image8} alt='' />
+            </div>
+            
+        </div>
+    ),
+    tab2: (
+        <div className=' pt-4 flex flex-wrap justify-center items-center gap-4'>
+            <div className='border-[#d19d64] bg-[#d19d64] '>
+            <img className='w-[250px] h-[250px] object-cover hover:scale-95 hover:duration-500 hover:opacity-50 ' src={image9} alt='' />
+            </div>
+            <div className='border-[#d19d64] bg-[#d19d64] '>
+            <img className='w-[250px] h-[250px] object-cover hover:scale-95 hover:duration-500 hover:opacity-50 ' src={image10} alt='' />
+            </div>
+            <div className='border-[#d19d64] bg-[#d19d64] '>
+            <img className='w-[250px] h-[250px] object-cover hover:scale-95 hover:duration-500 hover:opacity-50 ' src={image11} alt='' />
+            </div>
+            <div className='border-[#d19d64] bg-[#d19d64] '>
+            <img className='w-[250px] h-[250px] object-cover hover:scale-95 hover:duration-500 hover:opacity-50 ' src={image12} alt='' />
+            </div>
+            <div className='border-[#d19d64] bg-[#d19d64] '>
+            <img className='w-[250px] h-[250px] object-cover hover:scale-95 hover:duration-500 hover:opacity-50 ' src={image13} alt='' />
+            </div>
+            <div className='border-[#d19d64] bg-[#d19d64] '>
+            <img className='w-[250px] h-[250px] object-cover hover:scale-95 hover:duration-500 hover:opacity-50 ' src={image14} alt='' />
+            </div>
+            <div className='border-[#d19d64] bg-[#d19d64] '>
+            <img className='w-[250px] h-[250px] object-cover hover:scale-95 hover:duration-500 hover:opacity-50 ' src={image15} alt='' />
+            </div>
+            <div className='border-[#d19d64] bg-[#d19d64] '>
+            <img className='w-[250px] h-[250px] object-cover hover:scale-95 hover:duration-500 hover:opacity-50 ' src={image16} alt='' />
+            </div>
+        </div>
     )
 }
+
+
+const Gallery = () => {
+    
+  const [activeTab, setActiveTab] = useState("tab1")
+
+
+    return (
+      <div className='h-[750px] w-full'>
+        <h2 className='text-4xl text-center font-oswald font-bold pt-20 pb-2 '>GALLERY</h2>
+        <img className='w-[80px] mx-auto pb-10' src='./barber-scissors.png' />
+        <div className='w-[90%] h-[80%] m-auto'>
+        <div className='text-center'>
+            {galleryTabs.map((tab)=> (
+                <button key={tab.id} onClick={()=> setActiveTab(tab.id)} className={`font-oswald px-4 mx-2 mt-2 font-light ${
+                    activeTab === tab.id ? 'bg-black text-white' : 'bg-white text-black'
+                }`}>
+                    {tab.label}
+                    </button>
+            ))}
+        </div>
+        <div>
+            {tabContent[activeTab]}
+        </div>
+        </div>
+       
+      </div>
+    );
+  }
 export default Gallery
