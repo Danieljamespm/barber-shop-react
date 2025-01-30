a6c70d37a3994a8d34a54d5e0cb48cd2cfa3bcaa
Danny Mettler <danieljamespm@gmail.com>
2025-01-30 13:36:09 -0500

    added photos to gallery component.

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
+import image1 from '../assets/haircut-1.jpg'
+import image2 from '../assets/haircut-2.jpg'
+import image3 from '../assets/haircut-3.jpg'
+import image4 from '../assets/haircut-4.jpg'
+import image5 from '../assets/haircut-5.jpg'
+import image6 from '../assets/haircut-6.jpg'
+import image7 from '../assets/haircut-7.jpg'
+import image8 from '../assets/haircut-8.jpg'
+import image9 from '../assets/shave-1.jpg'
+import image10 from '../assets/shave-2.jpg'
+import image11 from '../assets/shave-3.jpg'
+import image12 from '../assets/shave-4.jpg'
+import image13 from '../assets/shave-5.jpg'
+import image14 from '../assets/shave-6.jpg'
+import image15 from '../assets/shave-7.jpg'
+import image16 from '../assets/shave-8.jpg'
+
 
 const Gallery = () => {
-  return (
-    <div>Gallery</div>
-  )
+    return(
+        <></>
+    )
 }
-
 export default Gallery
