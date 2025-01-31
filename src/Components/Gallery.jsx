d75e8bec70529c04e943a2129ccdb716864ff0e0
Danny Mettler <danieljamespm@gmail.com>
2025-01-30 21:01:17 -0500

    added photos as background images and added hover animation to darken image and add a gold border.

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -1,20 +1,6 @@
 import React, {useState} from 'react'
 
-import image2 from '../assets/haircut-2.jpg'
-import image3 from '../assets/haircut-3.jpg'
-import image4 from '../assets/haircut-4.jpg'
-import image5 from '../assets/haircut-5.jpg'
-import image6 from '../assets/haircut-6.jpg'
-import image7 from '../assets/haircut-7.jpg'
-import image8 from '../assets/haircut-8.jpg'
-import image9 from '../assets/shave-1.jpg'
-import image10 from '../assets/shave-2.jpg'
-import image11 from '../assets/shave-3.jpg'
-import image12 from '../assets/shave-4.jpg'
-import image13 from '../assets/shave-5.jpg'
-import image14 from '../assets/shave-6.jpg'
-import image15 from '../assets/shave-7.jpg'
-import image16 from '../assets/shave-8.jpg'
+
 
 
 const galleryTabs = [
@@ -24,24 +10,28 @@ const galleryTabs = [
 
 const tabContent = {
     tab1: (
-       <div className=' grid grid-cols-4 grid-rows-2 gap-2 w-full max-w-screen-lg mx-auto border border-gray-400'>
-        <div className='aspect-square bg-haircut-1 bg-cover bg-center'></div>
-        <div className='aspect-square bg-haircut-2 bg-cover bg-center'></div>
-        <div className='aspect-square bg-haircut-3 bg-cover bg-center'></div>
-        <div className='aspect-square bg-haircut-4 bg-cover bg-center'></div>
-        <div className='aspect-square bg-haircut-5 bg-cover bg-center'></div>
-        <div className='aspect-square bg-haircut-6 bg-cover bg-center'></div>
-        <div className='aspect-square bg-haircut-7 bg-cover bg-center'></div>
-        <div className='aspect-square bg-haircut-8 bg-cover bg-center'></div>
+       <div className=' grid grid-cols-4 grid-rows-2 gap-2 w-full max-w-screen-lg mx-auto border '>
+        <div className='aspect-square bg-haircut-1 bg-cover bg-center hover:border-4 hover:border-[#d19d64] hover:bg-black hover:bg-blend-darken hover:bg-opacity-50 hover:duration-500 hover:scale-105'></div>
+        <div className='aspect-square bg-haircut-2 bg-cover bg-center hover:border-4 hover:border-[#d19d64] hover:bg-black hover:bg-blend-darken hover:bg-opacity-50 hover:duration-500 hover:scale-105'></div>
+        <div className='aspect-square bg-haircut-3 bg-cover bg-center hover:border-4 hover:border-[#d19d64] hover:bg-black hover:bg-blend-darken hover:bg-opacity-50 hover:duration-500 hover:scale-105'></div>
+        <div className='aspect-square bg-haircut-4 bg-cover bg-center hover:border-4 hover:border-[#d19d64] hover:bg-black hover:bg-blend-darken hover:bg-opacity-50 hover:duration-500 hover:scale-105'></div>
+        <div className='aspect-square bg-haircut-5 bg-cover bg-center hover:border-4 hover:border-[#d19d64] hover:bg-black hover:bg-blend-darken hover:bg-opacity-50 hover:duration-500 hover:scale-105'></div>
+        <div className='aspect-square bg-haircut-6 bg-cover bg-center hover:border-4 hover:border-[#d19d64] hover:bg-black hover:bg-blend-darken hover:bg-opacity-50 hover:duration-500 hover:scale-105'></div>
+        <div className='aspect-square bg-haircut-7 bg-cover bg-center hover:border-4 hover:border-[#d19d64] hover:bg-black hover:bg-blend-darken hover:bg-opacity-50 hover:duration-500 hover:scale-105'></div>
+        <div className='aspect-square bg-haircut-8 bg-cover bg-center hover:border-4 hover:border-[#d19d64] hover:bg-black hover:bg-blend-darken hover:bg-opacity-50 hover:duration-500 hover:scale-105'></div>
 
        </div>
     ),
     tab2: (
-        <div className='bg-gray-500 grid grid-cols-4 w-full max-w-screen-lg mx-auto border border-gray-400'>
-        <div className='aspect-square bg-haircut-5 bg-cover bg-center'></div>
-        <div className='aspect-square bg-haircut-6 bg-cover bg-center'></div>
-        <div className='aspect-square bg-haircut-7 bg-cover bg-center'></div>
-        <div className='aspect-square bg-haircut-8 bg-cover bg-center'></div>
+        <div className=' grid grid-cols-4 gap-2 w-full max-w-screen-lg mx-auto border '>
+        <div className='aspect-square bg-shave-1 bg-cover bg-center hover:border-4 hover:border-[#d19d64] hover:bg-black hover:bg-blend-darken hover:bg-opacity-50 hover:duration-500 hover:scale-105'></div>
+        <div className='aspect-square bg-shave-2 bg-cover bg-center hover:border-4 hover:border-[#d19d64] hover:bg-black hover:bg-blend-darken hover:bg-opacity-50 hover:duration-500 hover:scale-105'></div>
+        <div className='aspect-square bg-shave-3 bg-cover bg-center hover:border-4 hover:border-[#d19d64] hover:bg-black hover:bg-blend-darken hover:bg-opacity-50 hover:duration-500 hover:scale-105'></div>
+        <div className='aspect-square bg-shave-4 bg-cover bg-center hover:border-4 hover:border-[#d19d64] hover:bg-black hover:bg-blend-darken hover:bg-opacity-50 hover:duration-500 hover:scale-105'></div>
+        <div className='aspect-square bg-shave-5 bg-cover bg-center hover:border-4 hover:border-[#d19d64] hover:bg-black hover:bg-blend-darken hover:bg-opacity-50 hover:duration-500 hover:scale-105'></div>
+        <div className='aspect-square bg-shave-6 bg-cover bg-center hover:border-4 hover:border-[#d19d64] hover:bg-black hover:bg-blend-darken hover:bg-opacity-50 hover:duration-500 hover:scale-105'></div>
+        <div className='aspect-square bg-shave-7 bg-cover bg-center hover:border-4 hover:border-[#d19d64] hover:bg-black hover:bg-blend-darken hover:bg-opacity-50 hover:duration-500 hover:scale-105'></div>
+        <div className='aspect-square bg-shave-8 bg-cover bg-center hover:border-4 hover:border-[#d19d64] hover:bg-black hover:bg-blend-darken hover:bg-opacity-50 hover:duration-500 hover:scale-105'></div>
 
        </div>
     )
@@ -54,11 +44,11 @@ const Gallery = () => {
 
 
     return (
-      <div className='bg-[#EAEFF2] h-[750px] w-full'>
+      <div className='bg-[#EAEFF2] mb-10 md:mb-20 w-full max-w-screen-xl'>
         <h2 className='text-4xl text-center font-oswald font-bold pt-20 pb-2 '>GALLERY</h2>
         <img className='w-[80px] mx-auto pb-10' src='./barber-scissors.png' />
-        <div className='  w-[90%] h-[80%] m-auto'>
-        <div className='text-center'>
+        <div className='  w-[90%] m-auto'>
+        <div className='text-center pb-5'>
             {galleryTabs.map((tab)=> (
                 <button key={tab.id} onClick={()=> setActiveTab(tab.id)} className={`font-oswald px-4 mx-2 mt-2 font-light ${
                     activeTab === tab.id ? 'bg-black text-white' : 'bg-white text-black'
