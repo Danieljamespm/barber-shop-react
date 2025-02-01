7cd82c96e8146ad975d05e249f808517ab4939fb
Danny Mettler <danieljamespm@gmail.com>
2025-02-01 11:37:10 -0500

    change list style font size for mobile and change icon color.

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,10 +5,10 @@ import { TiSocialFacebook, TiSocialInstagram, TiSocialTwitter, TiSocialYoutube, 
 
 const Footer = () => {
   return (
-    <div className='w-full h-[600px] bg-[#191919]'>
+    <div className='w-full h-[670px] bg-[#191919]'>
         <img src={Logo} alt='barbershop logo' className='w-32 mx-auto pt-10' />
         <div className='sm:flex justify-center'>
-            <ul className='text-white font-oswald text-[10px] font-light text-center pt-5 sm:flex sm:justify-around sm:w-[700px]'>
+            <ul className='text-white font-oswald text-sm font-light text-center pt-5 sm:flex sm:justify-around sm:w-[700px]'>
                 <li className='pb-2 hover:text-[#d19d64] hover:duration-500 hover:underline'><a href='#'>HOME</a></li>
                 <li className='pb-2 hover:text-[#d19d64] hover:duration-500 hover:underline'><a href='#aboutus'>ABOUT US</a></li>
                 <li className='pb-2 hover:text-[#d19d64] hover:duration-500 hover:underline'><a href='#services'>SERVICES</a></li>
@@ -24,26 +24,26 @@ const Footer = () => {
         <div className='pt-8'>
             <p className='text-center font-oswald text-white text-sm pb-2'>SUBSCRIBE TO US</p>
             <img className='w-[50px] mx-auto pb-2' src="./barber-scissors.png" alt="" />
-            <p className='text-center text-[10px] text-gray-500 pb-4'>Subscribe to our Newsletter to get a Gift Voucher</p>
+            <p className='text-center text-xs text-gray-500 pb-4'>Subscribe to our Newsletter to get a Gift Voucher</p>
             <div className='flex flex-row border border-gray-500 w-[320px] mx-auto '>
             <input type='email' placeholder='Enter your email' className='flex-grow bg-[#191919] text-[9px] py-2 indent-2 focus:outline-none caret-white font-Opensans' />
             <button className='bg-[#d19d64] w-[25px] pl-1 hover:bg-gray-500 hover:text-white'><FaEnvelopeOpen/></button>
             </div>
         </div>
         <div className='flex justify-evenly pt-5 w-[200px] mx-auto'>
-            <div className='w-6 h-6 bg-[#191919] border border-white rounded-xl flex justify-center items-center hover:bg-[#d19d64] cursor-pointer'>
+            <div className='w-6 h-6 bg-[#191919] border border-white rounded-xl flex justify-center items-center hover:bg-[#d19d64] cursor-pointer text-white'>
                 <TiSocialFacebook/>
             </div>
-            <div className='w-6 h-6 bg-[#191919] border border-white rounded-xl flex justify-center items-center hover:bg-[#d19d64] cursor-pointer'>
+            <div className='w-6 h-6 bg-[#191919] border border-white rounded-xl flex justify-center items-center hover:bg-[#d19d64] cursor-pointer text-white'>
                 <TiSocialInstagram/>
             </div>
-            <div className='w-6 h-6 bg-[#191919] border border-white rounded-xl flex justify-center items-center hover:bg-[#d19d64] cursor-pointer'>
+            <div className='w-6 h-6 bg-[#191919] border border-white rounded-xl flex justify-center items-center hover:bg-[#d19d64] cursor-pointer text-white'>
                 <TiSocialTwitter/>
             </div>
-            <div className='w-6 h-6 bg-[#191919] border border-white rounded-xl flex justify-center items-center hover:bg-[#d19d64] cursor-pointer'>
+            <div className='w-6 h-6 bg-[#191919] border border-white rounded-xl flex justify-center items-center hover:bg-[#d19d64] cursor-pointer text-white'>
                 <TiSocialGooglePlus/>
             </div>
-            <div className='w-6 h-6 bg-[#191919] border border-white rounded-xl flex justify-center items-center hover:bg-[#d19d64] cursor-pointer'>
+            <div className='w-6 h-6 bg-[#191919] border border-white rounded-xl flex justify-center items-center hover:bg-[#d19d64] cursor-pointer text-white'>
                 <TiSocialYoutube/>
             </div>
         </div>
