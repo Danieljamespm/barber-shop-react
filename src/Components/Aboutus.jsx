7fd32d8a698fbd6658804685a1d1285adf2eef75
Danny Mettler <danieljamespm@gmail.com>
2025-02-01 10:56:07 -0500

    updating font size for mobile.

--- src/Components/Aboutus.jsx
+++ src/Components/Aboutus.jsx
@@ -6,16 +6,16 @@ const Aboutus = () => {
         <div className='maw-w-[1240px] mx-auto grid md:grid-cols-2 font-oswald md:px-16'>
             <div className='flex flex-col justify-center'>
  
-            <p className='text-[15px] text-[#D19D64] pt-10 mb-2 md:py-2'>THE UNIQUE STORY</p>
+            <p className='text-xl text-[#D19D64] pt-10 mb-2 md:py-2'>THE UNIQUE STORY</p>
             
-            <h1 className='text-2xl sm:text-5xl font-bold'>52 YEARS OF EXPERIENCE</h1>
+            <h1 className='text-3xl sm:text-5xl font-bold'>52 YEARS OF EXPERIENCE</h1>
             <hr className='w-20 h-1 bg-[#D19D64] border-0 rounded mb-10 mt-8' />
 
           
             
-            <p className='text-md pb-8 text-balance tracking-widest uppercase text-clip'>Welcome to Vintage Cuts, where tradition meets precision. With over 52 years of expertise, we take pride in offering a timeless experience in men's grooming. Specializing in vintage haircuts and expert beard trims, our skilled barbers combine old-school techniques with a modern touch, ensuring every visit feels like stepping back into an era of refined craftsmanship.</p>
+            <p className='text-lg pb-8 text-balance tracking-widest uppercase text-clip'>Welcome to Vintage Cuts, where tradition meets precision. With over 52 years of expertise, we take pride in offering a timeless experience in men's grooming. Specializing in vintage haircuts and expert beard trims, our skilled barbers combine old-school techniques with a modern touch, ensuring every visit feels like stepping back into an era of refined craftsmanship.</p>
             
-            <p className='text-sm text-balance tracking-widest pb-10 font-Opensans font-light'>Since opening our doors, we’ve built a reputation not just for skill, but for an atmosphere where authenticity, attention to detail, and true craftsmanship reign supreme. Whether you're looking for a classic pompadour, a clean fade, or a beard that’s sharp and well-groomed, we’ve got you covered. Our barbers are passionate about their craft, trained in vintage styles while always staying current with the latest trends. At Vintage Cuts, we don’t just cut hair; we create lasting impressions.
+            <p className='text-md text-balance tracking-widest pb-10 font-Opensans font-light'>Since opening our doors, we’ve built a reputation not just for skill, but for an atmosphere where authenticity, attention to detail, and true craftsmanship reign supreme. Whether you're looking for a classic pompadour, a clean fade, or a beard that’s sharp and well-groomed, we’ve got you covered. Our barbers are passionate about their craft, trained in vintage styles while always staying current with the latest trends. At Vintage Cuts, we don’t just cut hair; we create lasting impressions.
             <br/>
             Join us for a grooming experience that’s more than just a haircut – it’s a tradition.</p>
             </div>
@@ -25,7 +25,7 @@ const Aboutus = () => {
            
 
 
-        <button className='bg-[#D19D64] text-black px-6 py-2 m-5 md:mx-32 text-[9px] font-oswald  hover:bg-black hover:text-[#D19D64] hover:duration-500 rounded-lg font-bold tracking-widest'>READ MORE</button>
+        <button className='bg-[#D19D64] text-black px-7 py-3 m-5 md:mx-32 text-xs font-oswald  hover:bg-black hover:text-[#D19D64] hover:duration-500 rounded-lg font-bold tracking-widest'>READ MORE</button>
 
         
 
