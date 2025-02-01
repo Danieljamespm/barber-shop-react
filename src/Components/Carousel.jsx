2e003d199677c815b911624302b62c5c3cc99af4
Danny Mettler <danieljamespm@gmail.com>
2025-02-01 10:59:30 -0500

    changed padding and font size for mobile.

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -68,18 +68,18 @@ const services = [
       <h1 className='text-center pt-10 font-bold font-oswald text-4xl pb-2 '>SPECIAL SERVICES</h1>
       <img className='w-[80px] mx-auto' src="./barber-scissors.png" alt="" />
     <div className=' md:w-[1100px] m-auto'>
-      <div className='mt-20'>
+      <div className='mt-8'>
         <Slider {...settings}>
         {services.map((card) => (
-          <div className='bg-slate-200  md:h-[450px] h-[400px] border-[3px] border-black '>
+          <div className='bg-slate-200  md:h-[450px] h-[500px] border-[3px] border-black '>
             <div className='flex justify-center items-center'>
               <img src={card.image} alt='' className='w-[100%] pt-2 px-2 object-cover'/>
             </div>
 
             <div className='flex flex-col items-center justify-center gap-4 p-10 md:p-5 px-2 mx-2 hover:bg-black hover:text-white'>
-              <p className='text-[#D19D64] text-2xl font-oswald uppercase font-bold text-center px-2'>{card.title}</p>
-              <p className='text-[10px] px-2'>{card.description}</p>
-              <button className='bg-white border-[1px] border-black text-black px-10 py-2 m-5 md:mx-32 text-[8px] hover:bg-[#bb9e86] hover:text-white hover:duration-500 rounded-lg font-oswald font-bold tracking-widest text-nowrap'>READ MORE</button>
+              <p className='text-[#D19D64] text-4xl font-oswald uppercase font-bold text-center px-2'>{card.title}</p>
+              <p className='text-lg px-2'>{card.description}</p>
+              <button className='bg-white border-[1px] border-black text-black px-10 py-2 m-5 md:mx-32 text-sm hover:bg-[#bb9e86] hover:text-white hover:duration-500 rounded-lg font-oswald font-bold tracking-widest text-nowrap'>READ MORE</button>
             </div>
           </div>
         ))}
