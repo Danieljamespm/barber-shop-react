862efc8736db642a4f0d6ca00aa96dbf9416e86d
Danny Mettler <danieljamespm@gmail.com>
2025-02-01 15:15:17 -0500

    change font size for larger screens.

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -6,10 +6,10 @@ const Hero = () => {
     <div className='bg-hero-image bg-cover bg-center h-[850px] flex justify-center items-center flex-col font-oswald '>
  
         <img className='w-[220px]  mx-auto' src="./barber-scissors.png" alt="" />
-        <p className='text-lg md:text-sm text-white [word-spacing:0.16rem] p-2 tracking-widest font-light'>WE ARE A UNIQUE</p>
+        <p className='text-lg md:text-3xl text-white [word-spacing:0.16rem] p-2 tracking-widest font-light'>WE ARE A UNIQUE</p>
         <h1 className='text-white font-bold text-6xl sm:text-6xl md:text-9xl '>BARBER SHOP</h1>
-        <p className='text-white p-2 text-md font-light tracking-widest font-Opensans'>The best place for your hairstyle.</p>
-        <button className='bg-[#D19D64] p-3 m-4 text-[10px] hover:text-[#34567e] hover:duration-500 rounded-lg font-bold tracking-widest'>MAKE AN APPOINTMENT</button>
+        <p className='text-white p-2 text-md md:text-xl font-light tracking-widest font-Opensans'>The best place for your hairstyle.</p>
+        <button className='bg-[#D19D64] p-3 m-4 text-md hover:text-[#34567e] hover:duration-500 rounded-lg font-bold tracking-widest'>MAKE AN APPOINTMENT</button>
 
        
         <div>
