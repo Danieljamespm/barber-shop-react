11403d03f4117333e1dddba53c2f44cedd443508
Danny Mettler <danieljamespm@gmail.com>
2025-02-06 13:14:59 -0500

    updated font sizes for large and xl screens.

--- src/Components/Discount.jsx
+++ src/Components/Discount.jsx
@@ -15,7 +15,7 @@ const Discount = () => {
             <p className='md:text-xl lg:text-2xl font-bold'>We also do a drawing every month with the possibility of winning 25% off all services for a <span className='text-[#d19d64]'>WHOLE YEAR</span></p>
             <p className='md:text-xl lg:text-2xl font-bold'>There are 3 ways to enter and each one earns you 1 entry.</p>
             <div className='text-[#d19d64] w-full md:w-[35%] mx-auto pt-5'>
-            <ul className='text-justify list-disc list-inside'>
+            <ul className='text-justify list-disc list-inside text-sm lg:text-lg'>
                 <li className=''>Leave a 5 star review</li>
                 <li>Share a photo of your haircut and tag us! #VINTAGECUTS</li>
                 <li>Refer a friend <br></br> <span className='text-sm lg:text-lg'>(have them present a business card 
@@ -24,7 +24,7 @@ const Discount = () => {
             </div>
         </div>
         <div className='flex justify-center'>
-        <button className='bg-[#D19D64] p-4 md:p-3 md:m-6 m-10 mx-auto text-xs lg:text-xl hover:text-[#d19d64] hover:bg-black hover:duration-500 rounded-lg font-bold font-oswald tracking-wide'>MAKE AN APPOINTMENT NOW</button>
+        <button className='bg-[#D19D64] p-4 md:p-3 md:m-6 m-10 mx-auto text-xs lg:text-md xl:text-lg hover:text-[#d19d64] hover:bg-black hover:duration-500 rounded-lg font-bold font-oswald tracking-wide'>MAKE AN APPOINTMENT NOW</button>
         </div>
         </div>
     </div>
