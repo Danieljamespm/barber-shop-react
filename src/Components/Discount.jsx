2c1992a3d573d3f648d8df5dcb86c0e37bc40545
Danny Mettler <danieljamespm@gmail.com>
2025-02-06 12:05:50 -0500

    updated font sizes for responsiveness on large screens.

--- src/Components/Discount.jsx
+++ src/Components/Discount.jsx
@@ -7,13 +7,13 @@ const Discount = () => {
     <div className='bg-white h-[800px] sm:h-[700px] md:h-[700px] flex justify-center pt-24'>
         <div className='h-[42%] bg-[#EAEFF2] w-[92%] p-2  border-black border-2'>
         <div className='h-[100%] bg-black w-[100%] m-auto flex justify-center items-center flex-col '>
-            <p className='text-[#D19D64] font-oswald text-3xl md:text-5xl font-bold'>GET FLAT 25% DISCOUNT</p>
-            <p className='text-white text-2xl md:text-5xl font-oswald font-light'>ON EVERY SUNDAY OF DECEMBER</p>
-            <p className='text-white font-oswald p-4 text-xl md:xl'>USE PROMO CODE: <span className='text-[#d19d64]'>VINTAGEBARBER</span></p>
+            <p className='text-[#D19D64] font-oswald text-3xl md:text-5xl lg:text-6xl font-bold'>GET FLAT 25% DISCOUNT</p>
+            <p className='text-white text-2xl md:text-5xl lg:text-6xl font-oswald font-light'>ON EVERY SUNDAY OF DECEMBER</p>
+            <p className='text-white font-oswald p-4 text-xl md:text-2xl lg:text-3xl'>USE PROMO CODE: <span className='text-[#d19d64]'>VINTAGEBARBER</span></p>
         </div>
-        <div className=' font-oswald text-md w-full text-center pt-10'>
-            <p className='md:text-xl font-bold'>We also do a drawing every month with the possibility of winning 25% off all services for a <span className='text-[#d19d64]'>WHOLE YEAR</span></p>
-            <p className='md:text-xl font-bold'>There are 3 ways to enter and each one earns you 1 entry.</p>
+        <div className=' font-oswald text-md lg:text-2xl w-full text-center pt-10'>
+            <p className='md:text-xl lg:text-2xl font-bold'>We also do a drawing every month with the possibility of winning 25% off all services for a <span className='text-[#d19d64]'>WHOLE YEAR</span></p>
+            <p className='md:text-xl lg:text-2xl font-bold'>There are 3 ways to enter and each one earns you 1 entry.</p>
             <div className='text-[#d19d64] w-full md:w-[35%] mx-auto pt-5'>
             <ul className='text-justify list-disc list-inside'>
                 <li className=''>Leave a 5 star review</li>
@@ -24,7 +24,7 @@ const Discount = () => {
             </div>
         </div>
         <div className='flex justify-center'>
-        <button className='bg-[#D19D64] p-4 md:p-3 md:m-10 m-10 mx-auto text-xs hover:text-[#d19d64] hover:bg-black hover:duration-500 rounded-lg font-bold font-oswald tracking-wide'>MAKE AN APPOINTMENT NOW</button>
+        <button className='bg-[#D19D64] p-4 md:p-3 md:m-6 m-10 mx-auto text-xs lg:text-xl hover:text-[#d19d64] hover:bg-black hover:duration-500 rounded-lg font-bold font-oswald tracking-wide'>MAKE AN APPOINTMENT NOW</button>
         </div>
         </div>
     </div>
