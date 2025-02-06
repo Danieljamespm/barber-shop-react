cfdb2ab90dae53f30bc7a1c02be88900e96a0912
Danny Mettler <danieljamespm@gmail.com>
2025-02-06 12:29:12 -0500

    adjusted font sizes and div sizes for lager screens.

--- src/Components/Appointment.jsx
+++ src/Components/Appointment.jsx
@@ -2,53 +2,53 @@ import React from 'react'
 
 const Appointment = () => {
   return (
-    <div  id='appointment' className=' w-full h-[1100px] sm:h-[700px] bg-black text-white'>
-        <h2 className='text-center pt-20 pb-2 text-4xl font-oswald font-bold '>MAKE AN APPOINTMENT</h2>
-        <img className='w-[80px] mx-auto' src='./barber-scissors.png' />
-        <div className=' w-full pt-7 sm:flex sm:justify-around px-4'>
-        <div className='pb-4 w-full sm:w-[300px] '>
-            <p className='font-oswald font-bold text-xl text-[#d19d64] pb-8'>OPENING HOURS</p>
+    <div  id='appointment' className=' w-full h-[1100px] lg:h-[900px] bg-black text-white'>
+        <h2 className='text-center pt-20 pb-2 text-4xl lg:text-6xl font-oswald font-bold '>MAKE AN APPOINTMENT</h2>
+        <img className='w-[80px] lg:w-[100px] mx-auto' src='./barber-scissors.png' />
+        <div className=' w-full pt-7 lg:pt-20 sm:flex sm:justify-around lg:justify-evenly px-4'>
+        <div className='pb-4 w-full sm:w-[300px] lg:w-[500px] '>
+            <p className='font-oswald font-bold text-xl lg:text-4xl text-[#d19d64] pb-8'>OPENING HOURS</p>
             <div className='flex justify-between'>
-            <p className='font-oswald text-xs font-light'>MONDAY</p>
-            <div className=' w-[200px] sm:w-[70%] border-b border-dotted pt-2'></div>
-            <p className='font-oswald text-xs font-light'>9AM-6PM</p>
+            <p className='font-oswald text-xs lg:text-xl font-light'>MONDAY</p>
+            <div className=' w-[200px] sm:w-[70%] border-b border-dotted pt-2 lg:border-b-2'></div>
+            <p className='font-oswald text-xs lg:text-xl font-light'>9AM-6PM</p>
             </div>
             <div className='flex justify-between'>
-            <p className='font-oswald text-xs pt-9 font-light '>TUESDAY</p>
-            <div className='w-[200px] sm:w-[70%] border-b border-dotted'></div>
-            <p className='font-oswald text-xs font-light pt-9'>9AM-6PM</p>
+            <p className='font-oswald text-xs pt-9 lg:text-xl font-light '>TUESDAY</p>
+            <div className='w-[200px] sm:w-[70%] border-b border-dotted lg:border-b-2'></div>
+            <p className='font-oswald text-xs lg:text-xl font-light pt-9'>9AM-6PM</p>
             </div>
             <div className='flex justify-between'>
-            <p className='font-oswald text-xs pt-9 font-light '>WEDNESDAY</p>
-            <div className='w-[200px]  sm:w-[65%] border-b border-dotted'></div>
-            <p className='font-oswald text-xs font-light pt-9'>9AM-6PM</p>
+            <p className='font-oswald text-xs pt-9 lg:text-xl font-light '>WEDNESDAY</p>
+            <div className='w-[200px]  sm:w-[65%] border-b border-dotted lg:border-b-2'></div>
+            <p className='font-oswald text-xs lg:text-xl font-light pt-9'>9AM-6PM</p>
             </div>
             <div className='flex justify-between'>
-            <p className='font-oswald text-xs pt-9 font-light '>THURSDAY</p>
-            <div className='w-[200px] sm:w-[65%] border-b border-dotted'></div>
-            <p className='font-oswald text-xs font-light  pt-9'>9AM-6PM</p>
+            <p className='font-oswald text-xs pt-9 lg:text-xl font-light '>THURSDAY</p>
+            <div className='w-[200px] sm:w-[65%] border-b border-dotted lg:border-b-2'></div>
+            <p className='font-oswald text-xs lg:text-xl font-light  pt-9'>9AM-6PM</p>
             </div>
             <div className='flex justify-between'>
-            <p className='font-oswald text-xs pt-9 font-light '>FRIDAY</p>
-            <div className='w-[200px] sm:w-[70%] border-b border-dotted'></div>
-            <p className='font-oswald text-xs font-light  pt-9'>9AM-6PM</p>
+            <p className='font-oswald text-xs pt-9 lg:text-xl font-light '>FRIDAY</p>
+            <div className='w-[200px] sm:w-[70%] border-b border-dotted lg:border-b-2'></div>
+            <p className='font-oswald text-xs lg:text-xl font-light  pt-9'>9AM-6PM</p>
             </div>
             <div className='flex justify-between'>
-            <p className='font-oswald text-xs pt-9 font-light '>SATURDAY</p>
-            <div className='w-[200px] sm:w-[65%] border-b border-dotted'></div>
-            <p className='font-oswald text-xs font-light  pt-9'>10AM-4PM</p>
+            <p className='font-oswald text-xs pt-9 lg:text-xl font-light '>SATURDAY</p>
+            <div className='w-[200px] sm:w-[65%] border-b border-dotted lg:border-b-2'></div>
+            <p className='font-oswald text-xs lg:text-xl font-light  pt-9'>10AM-4PM</p>
             </div>
             <div className='flex justify-between'>
-            <p className='font-oswald text-xs pt-9 font-light '>SUNDAY</p>
-            <div className='w-[200px]  border-b border-dotted'></div>
-            <p className='font-oswald text-xs font-light  pt-9'>CLOSED</p>
+            <p className='font-oswald text-xs pt-9 lg:text-xl font-light '>SUNDAY</p>
+            <div className='w-[200px] lg:w-[400px] border-b border-dotted lg:border-b-2'></div>
+            <p className='font-oswald text-xs lg:text-xl font-light  pt-9'>CLOSED</p>
             </div>
         </div>
-        <div className='bg-[#d19d64] w-full sm:w-[300px] md:w-[400px] pb-10'>
-            <h3 className='font-oswald text-center pt-6 text-2xl font-bold'>BOOK NOW</h3>
-            <img className='w-[50px] mx-auto' src='./barber-scissors.png' />
+        <div className='bg-[#d19d64] w-full sm:w-[300px] md:w-[400px] lg:w-[500px] lg:h-[500px] pb-10'>
+            <h3 className='font-oswald text-center pt-6 text-2xl lg:text-4xl font-bold'>BOOK NOW</h3>
+            <img className='w-[50px] lg:w-[80px] mx-auto' src='./barber-scissors.png' />
             <div>
-                <form className='flex flex-col text-gray-600 items-center pt-4'>
+                <form className='flex flex-col text-gray-600 items-center pt-4 lg:pt-10'>
                     <input  className='bg-[#d19d64] border border-white w-[80%] mb-2 text-md font-oswald py-1 placeholder-gray-600 indent-2 font-light focus:outline-none'type='text' name='name' placeholder='Name' />
                     <input  className='bg-[#d19d64] border border-white w-[80%] mb-2 text-md font-oswald py-1 indent-2 placeholder-gray-600  focus:outline-none'type='email' name='email' placeholder='Email'  />
                     <input  className='bg-[#d19d64] border border-white w-[80%] mb-2 text-md font-oswald py-1 indent-2 placeholder-gray-600 focus:outline-none'type='number' name='phone-number' placeholder='Phone'/>
