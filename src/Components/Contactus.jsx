ad5d89de02eea7a0254077db6bab9539371a43d7
Danny Mettler <danieljamespm@gmail.com>
2025-02-06 14:27:23 -0500

    updated sizing for larger screens.

--- src/Components/Contactus.jsx
+++ src/Components/Contactus.jsx
@@ -3,12 +3,12 @@ import { FaClock, FaEnvelopeOpen, FaMapMarkerAlt, FaPhone } from "react-icons/fa
 
 const Contactus = () => {
   return (
-    <div id='contact' className='w-full h-[620px] sm:h-[300px] bg-contact-image bg-cover bg-center bg-black bg-blend-darken bg-opacity-70'>
-        <h2 className='text-white font-oswald text-3xl font-bold text-center pt-10 pb-2'>CONTACT US</h2>
-        <img className='w-[80px] mx-auto pb-7' src='./barber-scissors.png' />
-        <div className='sm:flex justify-around'>
+    <div id='contact' className='w-full h-[620px] sm:h-[500px] bg-contact-image bg-cover bg-center bg-black bg-blend-darken bg-opacity-70'>
+        <h2 className='text-white font-oswald text-3xl md:text-6xl font-bold text-center pt-10 md:pt-20 pb-2'>CONTACT US</h2>
+        <img className='w-[80px] md:w-[100px] mx-auto pb-7' src='./barber-scissors.png' />
+        <div className='sm:flex justify-around md:pt-10'>
             <div  className='pb-8'>
-                <div className='w-7 h-7 bg-white flex justify-center items-center rounded-xl m-auto'>
+                <div className='w-7 h-7 md:w-10 md:h-10 bg-white flex justify-center items-center rounded-full m-auto'>
                 <FaMapMarkerAlt className='' />
                 </div>
                 <div className='text-center'>
@@ -17,7 +17,7 @@ const Contactus = () => {
                 </div>
             </div>
             <div className='pb-8'>
-                <div className='w-7 h-7 bg-white flex justify-center items-center rounded-xl m-auto'>
+                <div className='w-7 h-7 md:w-10 md:h-10 bg-white flex justify-center items-center rounded-full m-auto'>
                 <FaPhone className='' />
                 </div>
                 <div className='text-center'>
@@ -26,7 +26,7 @@ const Contactus = () => {
                 </div>
             </div>
             <div className='pb-8'>
-                <div className='w-7 h-7 bg-white flex justify-center items-center rounded-xl m-auto'>
+                <div className='w-7 h-7 md:w-10 md:h-10 bg-white flex justify-center items-center rounded-full m-auto'>
                 <FaEnvelopeOpen />
                 </div>
                 <div className='text-center'>
@@ -35,7 +35,7 @@ const Contactus = () => {
                 </div>
             </div>
             <div>
-                <div className='w-7 h-7 bg-white flex justify-center items-center rounded-xl m-auto'>
+                <div className='w-7 h-7 md:w-10 md:h-10 bg-white flex justify-center items-center rounded-full m-auto'>
                 <FaClock />
                 </div>
                 <div className='text-center'>
