696aea493326eb29bc1b0dd46713b2fd94d5dfc6
Danny Mettler <danieljamespm@gmail.com>
2025-03-31 14:22:52 -0400

    added padding to image to move everything down a little for mobile.

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className='bg-hero-image bg-cover bg-center h-[400px] sm:h-[800px] lg:h-[1200px] xl:h-[1400px] bg-black bg-blend-darken bg-opacity-30 flex justify-center items-center flex-col font-oswald '>
  
-        <img className=' w-[100px] md:w-[180px] lg:w-[220px] mx-auto' src="./barber-scissors.png" alt="" />
+        <img className=' w-[100px] pt-20 md:w-[180px] lg:w-[220px] mx-auto' src="./barber-scissors.png" alt="" />
         <p className='text-lg md:text-3xl text-white [word-spacing:0.16rem] p-2 tracking-widest font-light'>WE ARE A UNIQUE</p>
         <h1 className='text-white font-bold text-6xl sm:text-6xl md:text-7xl lg:text-9xl '>BARBER SHOP</h1>
         <p className='text-white p-2 text-md md:text-xl font-light tracking-widest font-Opensans'>The best place for your hairstyle.</p>
